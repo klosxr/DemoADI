0f41e0570a84371d6722a1c7b78de76d94fc1987
Francisco Molina <81399758+klosxr@users.noreply.github.com>
2025-05-25 23:16:02 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Subiendo imagenes (script.sh)"
+
+#ejecutar script.sh
+./script.sh
+
+#esperar 10 segundos
+sleep 10
+
+echo "Desplegando la aplicacion en la plataforma (script2.sh)"
+
+#ejecutar script2.sh
+./script2.sh
